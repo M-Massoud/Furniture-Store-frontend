f167a83cb9f127fa59fea5182e02c4683489d0e1
Ahmed Ismail <ahmedmhmd76@gmail.com>
2022-08-04 12:04:49 +0200

    fix private route props bug

--- src/components/privateRoute/PrivateRoute.js
+++ src/components/privateRoute/PrivateRoute.js
@@ -1,7 +1,7 @@
 import { Route, Redirect, useHistory, useLocation } from 'react-router-dom';
 import jwt from 'jwt-decode';
 
-export default function PrivateRoute({ children, requiredRole, ...rest }) {
+export default function PrivateRoute({ component: Component, requiredRole, ...rest }) {
 
     const location = useLocation();
     const history = useHistory();
@@ -18,20 +18,18 @@ export default function PrivateRoute({ children, requiredRole, ...rest }) {
     // get id from url
     const requestedId = location.pathname.split('/')[2];
     return (
-        <Route
-            {...rest}
-            render={({ location }) => {
-                return (
-                    (token !== "unAuthenticated") &&
-                    (requiredRole === 'userById' ?
-                        ((token?.role === 'user') && (token?.id === Number(requestedId))) :
-                        (token?.role === requiredRole))) ?
-                    children :
-                    ((token?.role === 'user') && (token?.id !== requestedId)) ?
-                        <h1>Un Authorized Access</h1> :
-                        <Redirect to={{ pathname: requiredRole !== 'admin' ? '/login-user' : '/login-admin', state: { from: location } }} />
-                    ;
-            }}
+        <Route {...rest} render={(props) => {
+            return (
+                (token !== "unAuthenticated") &&
+                (requiredRole === 'userById' ?
+                    ((token?.role === 'user') && (token?.id === Number(requestedId))) :
+                    (token?.role === requiredRole))) ?
+                <Component {...props} /> :
+                ((token?.role === 'user') && (token?.id !== requestedId)) ?
+                    <h1>Un Authorized Access</h1> :
+                    <Redirect to={{ pathname: requiredRole !== 'admin' ? '/login-user' : '/login-admin' }} />
+                ;
+        }}
         />
     );
 }
