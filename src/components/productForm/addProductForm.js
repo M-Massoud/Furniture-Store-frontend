557a70db81ac7325cb38c3e67cba002188c9d89b
M-Massoud <mohamed97masoud97@gmail.com>
2022-08-05 21:19:42 +0200

    improve edit and add product forms

--- src/components/productForm/addProductForm.js
+++ src/components/productForm/addProductForm.js
@@ -2,8 +2,11 @@ import { useState, useEffect } from 'react';
 import axiosInstance from '../../network/Config';
 function AddProductForm(props) {
 
-    const [productImg, setProductImg] = useState(null)
+    const [productImg, setProductImg] = useState("default-product-img.jpg")
     const [subCategoriesData, setSubCategoriesData] = useState([])
+    const [showSuccessAlert,setShowSuccessAlert]=useState(false)
+    const [showErrorAlert,setShowErrorAlert]=useState(false)
+    // console.log(subCategoriesData);
     let formData = new FormData();
 
     const [formDetails, setFormDetails] = useState({
@@ -12,9 +15,9 @@ function AddProductForm(props) {
         productSubcategory: "",
         productDescription: "",
         productImage: "",
-        productAmount: "",
+        productAmount: 2,
         productPrice: "",
-        productDiscount: ""
+        productDiscount: 0
     });
 
     const [formError, setFormerror] = useState({
@@ -30,7 +33,7 @@ function AddProductForm(props) {
     });
 
     function handelFormchange(e) {
-        if (e.target.id == 'subCategory') {
+        if (e.target.id === 'subCategory') {
             setFormDetails({
                 ...formDetails,
                 subCategoryId: subCategoriesData[e.target.value]._id,
@@ -62,9 +65,39 @@ function AddProductForm(props) {
             .catch(err => console.log(err));
     }, []);
 
+
+// validate the required values on submit
+   const valdiateForm =  (e)=>{
+    
+     if(!formDetails.productName)
+     setFormerror({
+        ...formError,
+        productNameError: "This field is required"
+     });
+     if(!formDetails.productDescription)
+     setFormerror({
+        ...formError,
+        productDescriptionError: "This field is required"
+     });
+     if(!formDetails.productPrice)
+     setFormerror({
+        ...formError,
+        productPriceError: "This field is required"
+     });
+   }
+  
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        axiosInstance
+        valdiateForm();
+
+       // return if there's any errors
+        for (let index = 0; index <  Object.values(formError).length; index++) {
+           if( Object.values(formError)[index] ) return          
+        }
+
+        // console.log(formError);
+        await  axiosInstance
             .post('/products', formData,
                 {
                     headers: {
@@ -73,11 +106,18 @@ function AddProductForm(props) {
                     "Content-Type": "multipart/form-data",
                 }
             )
-            .then(res => console.log(res))
-            .catch(error => console.log(error));
+            .then(res => { console.log(res);
+                setShowSuccessAlert(true)
+                hideAlertMsg()
+            })
+            .catch(error => { console.log(error)
+                setShowErrorAlert(true)
+                hideAlertMsg()
+            });
     };   //handleSubmit function
 
     const addingProduct = (e) => {
+        valdiateForm();
         formData.append("name", formDetails.productName);
         formData.append("description", formDetails.productDescription);
         formData.append("stockAmount", formDetails.productAmount);
@@ -89,79 +129,90 @@ function AddProductForm(props) {
     }  // Add product func
 
 
+    // alert feedback 
+    const successAlert=function(e){
+        return <div className="alert alert-success w-50 m-auto mt-2" role="alert">
+                    new product added successfully
+               </div>
+    }
+
+    const errorAlert=function(text){
+        return <div className="alert alert-danger w-50 m-auto  mt-2" role="alert">
+                    unexpected error
+               </div>
+    }
+
+    // to auto hide the alerts 
+    function hideAlertMsg(){
+    setTimeout(() => {
+        setShowErrorAlert(false)
+        setShowSuccessAlert(false)
+    }, 4000);
+  }
+
+    // regex to check if it only contains numbers 
+    const onlyNumbersRegex= new RegExp(/^\d+$/)
+
     /////////////////////////////////////////////
     const ErrorHandling = (input, value) => {
         switch (input) {
             case 'productName':
                 setFormerror({
                     ...formError,
-                    productNameError: value.length === 0 ? "This field is required" : "",
+                    productNameError: value.length === 0 ? "This field is required" : "" ||  onlyNumbersRegex.test(value) === true ?
+                    "product name can't only be numbers":""
                 });
                 break;
-            case 'subCategoryId':
-                setFormerror({
-                    ...formError,
-                    subCategoryIdError: value.length === 0 ? "This field is required" : "",
-                });
-                break;
+     
             case 'productSubcategory':
                 setFormerror({
                     ...formError,
                     productSubcategoryError: value.length === 0 ? "This field is required" : "",
                 });
                 break;
+
             case 'productDescription':
                 setFormerror({
                     ...formError,
-                    productDescriptionError: value.length === 0 ? "This field is required" : "",
+                    productDescriptionError: value.length === 0 ? "This field is required" : ""  ||
+                     onlyNumbersRegex.test(value) === true ?
+                    "product description can't only be numbers":""
                 });
                 break;
-            case 'productImage':
-                setFormerror({
-                    ...formError,
-                    productImageError: value.length === 0 ? "This field is required" : "",
-                });
-                break;
-            case 'productAmount':
-                setFormerror({
-                    ...formError,
-                    productAmountError: value.length === 0 ? "This field is required" : "",
-                });
-                break;
+         
             case 'productPrice':
                 setFormerror({
                     ...formError,
                     productPriceError: value.length === 0 ? "This field is required" : "",
                 });
                 break;
-            case 'productDiscount':
-                setFormerror({
-                    ...formError,
-                    productDiscountError: value.length === 0 ? "This field is required" : "",
-                });
-                break;
+         
             default:
                 setFormerror({
                     ...formError
                 })
         }  //value.length === 0 ? "This field is required" : "",
     }// ErrorHandling function
+
     ////////////////////////////////////////////////
     return (
-        <>
+        <>  
+            { showSuccessAlert === true && successAlert() }
+            { showErrorAlert === true && errorAlert() }
             <div className='col-10 offset-1 col-md-8 offset-md-2 border p-3 rounded shadow my-5'>
                 <h3 className='text-center mt-2'> Product Control </h3>
                 <form className={`co-12  row m-auto `} onSubmit={(e) => handleSubmit(e)}>
                     {/* //// */}
                     {/* //// */}
-                    <label htmlFor='productName' className="form-label mt-2"> Product Name</label >
-                    <input type='text' id={'productName'} name={'productName'} className={`form-control ${formError.productNameError && "border-danger"} `} onChange={(e) => handelFormchange(e)} />
+                    <label htmlFor='productName'  className="form-label mt-2"> Product Name</label >
+                    <input type='text'  id={'productName'} name={'productName'}className={`form-control ${formError.productNameError && "border-danger"} `} onChange={(e) => handelFormchange(e)} />
                     <div id="nameHelp" className="form-text text-danger">{formError.productNameError}</div>
                     {/* //// */}
                     <label htmlFor='productSubcategory' className="form-label mt-2"> Product subCategory</label >
                     <br />
-                    <select className={`form-control ${formError.productSubcategoryError && "border-danger"} `} id={'subCategory'} name={'subCategory'} onChange={(e) => handelFormchange(e)} required>
-                        <option value="" disabled>Select SubCategory</option>
+                    <select className={`form-control form-select ${formError.productSubcategoryError && "border-danger"} `} id={'subCategory'} name={'subCategory'} onChange={(e) => handelFormchange(e)} required>
+                        <option value="">Select SubCategory</option>
+                        
                         {subCategoriesData.map((subCategory, index) => {
                             return (
                                 <option key={subCategory._id} value={index}>{subCategory.title}</option>
@@ -190,7 +241,7 @@ function AddProductForm(props) {
 
                     {/* //// */}
                     <label htmlFor='productAmount' className="form-label  mt-2 "> Stock Amount</label >
-                    <input type='number' min={1} id='productAmount' name='productAmount' className={`form-control mb-2 ${formError.productAmountError && "border-danger"} `} onChange={(e) => handelFormchange(e)} />
+                    <input type='number' defaultValue={2}  id='productAmount' name='productAmount' className={`form-control mb-2 ${formError.productAmountError && "border-danger"} `} onChange={(e) => handelFormchange(e)} />
                     <div id="nameHelp" className="form-text text-danger">{formError.productAmountError}</div>
                     {/* //// */}
                     <label htmlFor='productPrice' className="form-label  mt-2 "> Product Price</label >
@@ -200,9 +251,8 @@ function AddProductForm(props) {
                     <label htmlFor='productDiscount' className="form-label  mt-2 "> Product Discount</label >
                     <input type='number' id='productDiscount' name='productDiscount' className={`form-control mb-2 ${formError.productDiscountError && "border-danger"} `} onChange={(e) => handelFormchange(e)} />
                     <div id="nameHelp" className="form-text text-danger">{formError.productDiscountError}</div>
-                    <div className='border col-10 offset-1 d-flex justify-content-evenly p-3 rounded shadow flex-wrap'>
-                        <button className='btn btn-primary col-8 mt-2 col-sm-5 col-md-3' onClick={() => addingProduct()}>Add Product</button>
-                    </div>
+              
+                    <button className='btn btn-primary mt-2 w-50 m-auto' onClick={() => addingProduct()}>Add Product</button>
                 </form>
 
             </div>
